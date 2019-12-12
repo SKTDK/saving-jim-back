e1a65feeb7fca1a0575339cd787a70a243782268
SKTDK <shopping@suyeon.org>
2019-12-13 00:11:04 +0100

    regex french names

--- modules/config.js
+++ modules/config.js
@@ -1,5 +1,5 @@
 // Regex for sanitizing inputs to prevent SQL injection
-const REGEX_USERNAME = /^([0-9]|[a-z]|[A-Z])+$/i;
+const REGEX_USERNAME = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/i;
 const REGEX_PASSWORD = /^([0-9]|[a-z]|[A-Z]|[!$.@#;:,])+$/i;
 const REGEX_NAME = /^([0-9]|[a-z]|[A-Z])+$/i;
 
